3114f616ecabf13afe020947221a8ebcbf66836e
johnangelomikhailareveche@iskolarngbayan.pup.edu.ph <mikhjnelo@gmail.com>
2025-06-15 13:21:29 +0800

    Cleaned the Directories, added Instructions in Notes and fixed the Image Import bug.

--- main/main.py
+++ main/main.py
@@ -8,21 +8,22 @@ import sqlite3
 connection = sqlite3.connect('../HotelManagementDB/HotelManagement.db')
 
 # SQL Statement
-strSQL = "SQL Statement Here!"
+#strSQL = "SQL Statement Here!"
 
 # Execute methods that run the SQL Query
-cursor = connection.execute(strSQL)
+#cursor = connection.execute(strSQL)
 
 # Fetches/Gets/Retrieves all of the records on the database from the cursor
 # (Depending on the SQL Statement)
 # rows = Instance of a Record
-rows = cursor.fetchall()
+#rows = cursor.fetchall()
 
 
 # Closes the cursor that gets the SQL QUERIES and Holds the Record/List of Records
-cursor.close()
+#cursor.close()
 # Closes the connection to the Database
-connection.close()
+#connection.close()
 # ----------------- DATABASE -------------------------------
 
 # ------------------ START UI BELOW ------------------------
+
